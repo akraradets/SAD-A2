57a631a923663071f48d4ef52e46131a7051f843
akraradets <akraradets@gmail.com>
2019-02-19 19:14:57 +0700

    implement singleton into machine/vendingMachine

--- machine/vendingMachine.go
+++ machine/vendingMachine.go
@@ -2,16 +2,38 @@ package machine
 
 import (
     "fmt"
+	"sync"
 )
 
 type VendingMachine struct {
-    Name   string
+    name   string
+    count  int
     // LastName    string
     // TotalLeaves int
     // LeavesTaken int
 }
 
+var (
+    once sync.Once
+    instance VendingMachine
+)
+
 // constructor
+func New(name string) VendingMachine {
+	once.Do(func() {
+		instance = VendingMachine{
+            name: name, 
+            count: 0
+        }
+    })
+	return instance
+}
+
 func (m VendingMachine) Display() string {
 	return fmt.Sprintf("Machine '%s'", m.Name)
+}
+
+func (m VendingMachine) count() string {
+    m.count = m.count + 1
+    return m.count
 }
--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
